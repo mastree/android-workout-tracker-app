9cc43b29aa1e83af4f68b8511ecfd5cae06cd0f2
Kamal S <muha.kamal@gmail.com>
2021-04-28 14:00:22 +0700

    add bottom navigation

--- app/src/main/java/com/saraga/workoutapp/MainActivity.kt
+++ app/src/main/java/com/saraga/workoutapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.saraga.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavMain)
+        val navController = findNavController(R.id.fragmentMain)
+        bottomNav.setupWithNavController(navController)
     }
 }
